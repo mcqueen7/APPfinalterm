83fb7e76c48c48d256d80a362a3e380c290f75ad
mqueen <chqueen.tw@gmail.com>
2022-04-10 15:57:45 +0800

    8boxes test

--- src/components/Home8Boxes.js
+++ src/components/Home8Boxes.js
@@ -4,21 +4,9 @@ import {Box,Button,Center,HStack,Text, VStack} from "native-base"
 import { ImageBackground } from "react-native";
 
 const Home8Boxes = ({boxdata}) => {
-    const image = { uri:`${boxdata.bg}` };
     return( 
-        <Box mt="25" w="400" maxW="100%" h="230" bgColor="#C8C8C8" alignItems="center" justifyContent="center">
-          <ImageBackground source={image} resizeMode="cover" style={styles.bg}>
-          <HStack  alignContent="center">
-            <VStack ml="16px">
-              <Text fontSize={16}>{boxdata.title}</Text>
-              <Box borderColor="black" borderTopWidth={1} borderBottomWidth={1} w="180" h="5px"></Box>
-            </VStack>
-            <VStack flexWrap="wrap" ml="30">
-              <Text w="160" fontSize={12}>{boxdata.content}</Text>
-              <Button w="110" h="40px" mt="15px" borderRadius={16} bgColor="#3B5998" >{boxdata.button}</Button>
-            </VStack>
-          </HStack>
-          </ImageBackground>
+        <Box mt="30" w="380" ml="10" maxW="100%" h="180" bgColor="blue.500" alignItems="center" justifyContent="center" flex={1} flexWrap="wrap">
+         
         </Box>
     );
 };
--- src/components/HomeAllList.js
+++ src/components/HomeAllList.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {  StyleSheet } from "react-native";
-import {NativeBaseProvider,SectionList,Heading,Center,Text,FlatList, } from 'native-base';
+import {NativeBaseProvider,SectionList,Heading,Center,Text,FlatList, VStack, HStack, } from 'native-base';
 
 import sections from "../json/home_all_section.json";
 // import sections from "../json/test.json";
 import HomeAllSchoolDetail from "./HomeAllSchoolDetail";
 import HomeBigBox1 from "./HomeBigBox1";
+import Home8Boxes from "./Home8Boxes";
 
 // console.log(sections);
 const HomeAllList = (navigation) => {
@@ -14,9 +15,9 @@ const HomeAllList = (navigation) => {
         case "big1":
             return null
         case "8boxes1":
-            return  <Heading fontSize="24" mt="8" pb="4">{section.title} </Heading>   
+            return  <HStack><Heading fontSize="24" mt="5" mr="230px">{section.title}</Heading><Heading fontSize="16" mt="5">More</Heading></HStack>
         case "8boxes2":
-            return  <Heading fontSize="24" mt="8" pb="4">{section.title} </Heading>  
+            return  <HStack><Heading fontSize="24" mt="7" mr="230px">{section.title}</Heading><Heading fontSize="16" mt="5">More</Heading></HStack>
         case "4boxes":
             return  <Center> <Heading fontSize="xl" mt="8" pb="4">FUCK2{section.title} </Heading> </Center>    
         case "big2":
@@ -33,7 +34,7 @@ const HomeAllList = (navigation) => {
         case "big1":
             return <HomeBigBox1 boxdata={item}/>
         case "8boxes1":
-            return null
+            return <Home8Boxes boxdata={item}/>
         case "8boxes2":
             return null
         case "4boxes":
