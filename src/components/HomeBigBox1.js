e9b6f4bcefd2e00b9750a830fcdccef4502e6854
mqueen <chqueen.tw@gmail.com>
2022-04-10 13:49:15 +0800

    big box1 commit

--- src/components/HomeBigBox1.js
+++ src/components/HomeBigBox1.js
@@ -7,10 +7,13 @@ const HomeBigBox1 = ({boxdata}) => {
     return( 
         <Box mt="25" w="400" maxW="100%" h="230" bgColor="blue.500" alignItems="center" justifyContent="center">
           <HStack  alignContent="center">
-            <Text>{boxdata.title}</Text>
+            <VStack >
+              <Text mr="10px" fontSize={16}>{boxdata.title}</Text>
+              <Box borderColor="black" borderTopWidth={1} borderBottomWidth={1} w="180" h="5px"></Box>
+            </VStack>
             <VStack flexWrap="wrap">
-            <Text >{boxdata.content}</Text>
-            <Button w="20" >item.button</Button>
+              <Text w="160" fontSize={12}>{boxdata.content}</Text>
+              <Button w="110" h="40px" mt="15px" borderRadius={16} bgColor="#3B5998" >{boxdata.button}</Button>
             </VStack>
           </HStack>
         </Box>
