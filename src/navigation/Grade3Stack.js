1b2a20533dbc6d6a42f711952bd5eec61ae39e29
yami <xxx@gmail>
2022-04-12 00:50:58 +0800

    Grade23stack

---
+++ src/navigation/Grade3Stack.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import {Box,HStack,Text} from "native-base"
+import HomeAllScreen from "../screens/HomeAllScreen"
+const Stack = createNativeStackNavigator(); 
+
+
+const Grade3Stack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="HomeAllscreen" 
+          component={HomeAllScreen}
+          options={
+            {headerShown:false}
+        }
+        
+        />
+      </Stack.Navigator>
+
+    );
+  }
+
+export default Grade3Stack;
+
