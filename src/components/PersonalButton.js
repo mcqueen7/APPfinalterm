87e561a007b0c696e1cfcba90bbb22ee5524894e
mqueen <chqueen.tw@gmail.com>
2022-04-12 00:13:07 +0800

    personal button

--- src/components/PersonalButton.js
+++ src/components/PersonalButton.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Box,Text,HStack, FlatList, Center, Image, Button, Icon, VStack } from "native-base";
+import { Box,Text,HStack, FlatList, Center, Image, Button, Icon, VStack ,Pressable} from "native-base";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const PersonalButton = ({ buttondata ,navigation }) => {
     
   return (
-        <Button bg={"#E8E8E8"} borderRadius={20}>
+    <Pressable onPress={()=>{alert("做不完了還MORE!!!!!!!!");}}>
+        <Button bg={"#E8E8E8"} borderRadius={20} w="160" mx={"10px"} my={"15px"}>
             <HStack>
-                <Text>asdasdasdsadasd</Text>
-                <MaterialCommunityIcons name={buttondata.icon} size={25} />
-                <Text>{buttondata.label}</Text>
+                <MaterialCommunityIcons name={buttondata.icon} size={25}/>
+                <Text w="60" textAlign={"center"}>{buttondata.label}</Text>
             </HStack>
         </Button>
+    </Pressable> 
   );
 };
 
