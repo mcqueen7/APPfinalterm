7b544f16949eb57817d51db5086bdd701a53ed6b
mqueen <chqueen.tw@gmail.com>
2022-04-13 23:34:21 +0800

    dark 2

--- src/components/Home8Boxes1.js
+++ src/components/Home8Boxes1.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { StyleSheet} from "react-native";
-import {Box,Button,Center,HStack,Text, VStack,Image} from "native-base"
+import {Box,Button,Center,HStack,Text, VStack,Image,useColorModeValue} from "native-base"
 import { ImageBackground } from "react-native";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 const Home8Boxes1 = ({boxdata,navigation}) => {
+    const textColor=useColorModeValue("#ffffff","#ffffff");
     return( 
         <Pressable onPress={()=>alert("做不完了沒Detail")}>
         <Center mt="20px" w="80px" mx="10px" h="80px" bgColor="#FFF1CD"  borderRadius={12} 
             shadow={7}>
                     <Image size={"50px"} resizeMode={"cover"} borderRadius={100} 
                     source={{uri: boxdata.image}} alt={boxdata.title} />
-                    <Text alignSelf={"center"} fontSize={12}>{boxdata.title}</Text>
+                    <Text alignSelf={"center"} fontSize={12} color={textColor}>{boxdata.title}</Text>
         </Center>
         </Pressable>
     );
