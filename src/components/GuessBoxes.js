c619203aa0cac625256097b8db077e28fc240039
yami <xxx@gmail>
2022-04-14 01:07:29 +0800

    gesdssboxcolor

--- src/components/GuessBoxes.js
+++ src/components/GuessBoxes.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Box, HStack, VStack, AspectRatio, Text, Image, Pressable,Center } from "native-base"
+import { Box, HStack, VStack, AspectRatio, Text, Image, Pressable,Center, useColorModeValue } from "native-base"
 
 const GuessDetail = ({ boxdata, navigation }) => {
+  const bgColor1=useColorModeValue("#FFF1CD","#EEEEEE")
   return (
-    <Center mb={7} mx={2} w="80px" h="60px"borderRadius={12} bgColor={"#FFF1CD"} shadow={7}> 
+    <Center mb={7} mx={2} w="80px" h="60px"borderRadius={12} bgColor={bgColor1} shadow={7}> 
      <Pressable onPress={() => {console.log("asdasdasda")}}>    
           <Text  
         _dark={{ color: "warmGray.700"}}
