b47e3bbc2a28d14d439e2f29ae4c0f2a26ed9864
mqueen <chqueen.tw@gmail.com>
2022-04-13 23:06:16 +0800

    person button dark

--- src/components/Home8Boxes2.js
+++ src/components/Home8Boxes2.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { StyleSheet} from "react-native";
-import {Box,Button,Center,HStack,Text, VStack,Image} from "native-base"
+import {Box,Button,Center,HStack,Text, VStack,Imagem,useColorModeValue} from "native-base"
 import { ImageBackground } from "react-native";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 const Home8Boxes2 = ({boxdata,navigation}) => {
+    const boxBGcolor=useColorModeValue("#92AFEC","#A4A2A2");
     const image = { uri:`${boxdata.image}` };
     return( 
         <Pressable onPress={()=>alert("做不完了沒Detail")}>
-        <Center mt="20px" w="80px" mx="10px" h="80px" bgColor="#92AFEC"  borderRadius={12} 
+        <Center mt="20px" w="80px" mx="10px" h="80px" bgColor={boxBGcolor}  borderRadius={12} 
             shadow={7}>
             <ImageBackground source={image} resizeMode="cover" style={styles.bg}>
               <Text alignSelf={"center"} fontSize={16}>{boxdata.title}</Text>
