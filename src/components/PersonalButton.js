4d01a04904aa10e740810ac5df4b369a6d45c9e6
mqueen <chqueen.tw@gmail.com>
2022-04-13 22:48:00 +0800

    persron darkmode 1

--- src/components/PersonalButton.js
+++ src/components/PersonalButton.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Box,Text,HStack, FlatList, Center, Image, Button, Icon, VStack ,Pressable} from "native-base";
+import { Box,Text,HStack, FlatList, Center, Image, Button, Icon, VStack ,Pressable,useColorMode} from "native-base";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+
+
 const PersonalButton = ({ buttondata ,navigation }) => {
-    
+  const{colorMode, toggleColorMode}=useColorMode();
   return (
         <Button bg={"#E8E8E8"} borderRadius={20} w="160" mx={"10px"} my={"15px"}
-          onPress={()=>{alert("做不完了還MORE!!!!!!!!");}}
+          onPress={()=>{
+            switch(buttondata.type){
+                case "darkmode":
+                  alert("switch darkmode");
+                  toggleColorMode();
+                  
+                  break;
+                default:   
+                  alert("做不完了還MORE!!!!!!!!");
+                  break;
+            }
+
+            
+          }}
         >
             <HStack>
                 <MaterialCommunityIcons name={buttondata.icon} size={25}/>
--- src/components/PersonalFile.js
+++ src/components/PersonalFile.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Box,Text,HStack, FlatList, Center, Image, Button, Icon } from "native-base";
+import { Box,Text,HStack, FlatList, Center, Image, Button, Icon,useColorModeValue } from "native-base";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const PersonalFile = ({ personaldata,navigation }) => {
-
+    const topBoxBGColor=useColorModeValue("#FFF5DB","#36322F");
   return (
-       <Box bgColor={"#FFF5DB"} w="100%" h="400px" alignItems={"center"} borderBottomLeftRadius={30} borderBottomRightRadius={30} mb="30px">
+       <Box bgColor={topBoxBGColor}w="100%" h="400px" alignItems={"center"} borderBottomLeftRadius={30} borderBottomRightRadius={30} mb="30px">
         <Image mt={2} source={{uri:personaldata.headimg}} alt={`${personaldata.name}的照片`} size={"120px"}/>
         <Text fontSize={20} fontWeight={"bold"} mt={"2"}>{personaldata.name}</Text>
-        <Text fontSize={16} color={"#868686"}>{personaldata.school}</Text>
+        <Text fontSize={16} _light={{color:"#868686"}} _dark={{color:"#B0B0B0"}}>{personaldata.school}</Text>
         <HStack mt={2}>
             <Center mx={5} w="70px">
                 <Text fontSize={16}>{personaldata.articleNum}</Text>
@@ -23,7 +23,9 @@ const PersonalFile = ({ personaldata,navigation }) => {
                 <Text fontSize={16}>被收藏數</Text>
             </Center>
         </HStack>
-        <Button mt={2} w="360" bgColor={"#477CEA"} borderRadius={"20px"} onPress={()=>alert("NO TIME")}>我的學習歷程</Button>
+        <Button mt={2} w="360" h={50} _light={{bgColor:"#477CEA"}} _dark={{bgColor:"#FFF5DB"}} borderRadius={"20px"} onPress={()=>alert("NO TIME")}>
+            <Text fontSize={16} _light={{color:"#ffffff"}} _dark={{color:"#000000"}}>我的學習歷程</Text>
+        </Button>
         <HStack mt={5}>
             <Center mx={7}>
                 <MaterialCommunityIcons name="account-multiple-outline" size={25} />
