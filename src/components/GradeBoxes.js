37616ada2800a6daf5fabfebd0a8d65db8afba2f
yami <xxx@gmail>
2022-04-14 00:19:17 +0800

    gradebox textcolor

--- src/components/GradeBoxes.js
+++ src/components/GradeBoxes.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { StyleSheet} from "react-native";
-import {Box,Button,Center,HStack,Text, VStack,Image,Pressable} from "native-base"
+import {Box,Button,Center,HStack,Text, VStack,Image,Pressable, useColorModeValue} from "native-base"
 import { ImageBackground } from "react-native";
 
 
 const GradeBoxes = ({boxdata,navigation}) => {
+  const textColor1=useColorModeValue("#000000","#000000");
     return( 
         <Pressable onPress={()=>alert("做不完了沒Detail")}>
         <Center mt="20px" w="160px" mx="15px" h="160px" bgColor="#92AFEC"  borderRadius={12} 
             shadow={7}>
             <ImageBackground source={{uri:boxdata.image}} resizeMode="cover" style={styles.bg} >
               <Center mt="100px" w="130" h="30px" backgroundColor={"#FFF1CD"} borderRadius={15} >
-                <Text textAlign={"center"} fontSize={12} >{boxdata.label}</Text>
+                <Text textAlign={"center"} fontSize={12} color={textColor1}>{boxdata.label}</Text>
               </Center>
             </ImageBackground>
         </Center>
